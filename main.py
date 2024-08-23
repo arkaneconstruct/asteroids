e0e7c9599880f0a79ff41fa2476e12f858d9820d
arkaneconstruct <justin.m.urena@gmail.com>
2024-08-23 01:29:44 -0400

    3. add game_tick, dt, and base sprite class.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import*
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while 1:
         for event in pygame.event.get():
@@ -10,6 +12,8 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
 
 
 
